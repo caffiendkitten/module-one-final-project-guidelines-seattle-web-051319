3b3fb71865d8b9a04262a5b56c646647eddc786d
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-05-28 15:58:47 -0700

    added user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190528221718) do
+ActiveRecord::Schema.define(version: 20190528225356) do
 
   create_table "jokes", force: :cascade do |t|
     t.string "punchline"
     t.string "category"
     t.string "setup"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
